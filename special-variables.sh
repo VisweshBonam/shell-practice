5eb4388e223718464c7e60f0fdfb0289aaed882e
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-16 13:15:14 +0530

    executing special variables

---
+++ special-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "variables passed to script: $@"
+
+echo "No of variables passed to script : $#"
+
+echo "Present working directory of the script executer : $PWD"
+
+echo "Present home directory of user , who is running the script : $HOME"
+
+echo "Which user is running the script : $USER"
+
+echo "PID of the script : $$"
+
+echo "PID of the last command in background : $!"
