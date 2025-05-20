5ece5351d4bd22aebb9d5bf958cc02a0ed9293ec
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-20 10:23:03 +0530

    check

--- loops.sh
+++ loops.sh
@@ -13,7 +13,8 @@ PACKAGES=("mysql" "python" "nginx" "httpd")
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-if [ $USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]
+then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
     exit 1 #give other than 0 upto 127
 else
@@ -21,8 +22,9 @@ else
 fi
 
 # validate functions takes input as exit status, what command they tried to install
-VALIDATE() {
-    if [ $1 -eq 0 ]; then
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
         echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
         echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
@@ -30,9 +32,12 @@ VALIDATE() {
     fi
 }
 
-for package in ${PACKAGES[@]}; do
+#for package in ${PACKAGES[@]}
+for package in $@
+do
     dnf list installed $package &>>$LOG_FILE
-    if [ $? -ne 0 ]; then
+    if [ $? -ne 0 ]
+    then
         echo "$package is not installed... going to install it" | tee -a $LOG_FILE
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "$package"
@@ -40,3 +45,4 @@ for package in ${PACKAGES[@]}; do
         echo -e "Nothing to do $package... $Y already installed $N" | tee -a $LOG_FILE
     fi
 done
+
