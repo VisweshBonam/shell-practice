40add8da0986a0a4a122f020ba443771c155b421
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-26 19:49:10 +0530

    commit all

---
+++ 18.backup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+UserId="$(id -u)"
+
+#Colors
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+
+
+echo "Scripted started executing at : $(date)"
+
+mkdir -p $LOG_FOLDER
+
+if [ $UserId -ne 0 ]
+then
+    echo -e "$R ERROR $N:: Please access with root user" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$G You are running with root $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is .....$G Success $N"
+    else
+        echo -e "$2 is .....$R Failed $N"
+        exit 1
+    fi 
+}
+
+VALIDATEARG(){
+    echo -e "$R USAGE:: $N sh 18-backup.sh <source-dir> <destination-dir> <days(optional)>"
+}
+
+if [ $# -lt 2 ]
+then
+    VALIDATEARG
+fi
+
+
+
