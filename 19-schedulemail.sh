df79c830d3c91a0a50630d9ad833fde244ed1094
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-27 11:40:12 +0530

    commit all

--- 19-schedulemail.sh
+++ 19-schedulemail.sh
@@ -7,10 +7,10 @@ MSG=""
 
 while IFS= read -r line
 do
-    CONSUMED_PERCENTAGE="$(echo line | awk '{print $6F}' | cut -d "%" -f1 )"
-    CONSUMED_DIR="$(echo line | awk '{print $7F}')"
+    CONSUMED_PERCENTAGE="$(echo $line | awk '{print $6F}' | cut -d "%" -f1 )"
+    CONSUMED_DIR="$(echo $line | awk '{print $7F}')"
 
-    if [ $CONSUMED_PERCENTAGE -ge $MAX_USAGE ]
+    if [ $CONSUMED_PERCENTAGE -gt $MAX_USAGE ]
     then
         $MSG += $CONSUMED_DIR is consumed more than 80%
     fi
