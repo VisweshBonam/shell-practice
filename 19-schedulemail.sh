fed000efa79f100aad6577a0ff42fbff57810160
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-27 12:04:47 +0530

    commit all

--- 19-schedulemail.sh
+++ 19-schedulemail.sh
@@ -3,6 +3,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 # in project it will be 75
 MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do
@@ -14,3 +15,4 @@ do
     fi
 done <<< $DISK_USAGE
 
+echo $MSG
