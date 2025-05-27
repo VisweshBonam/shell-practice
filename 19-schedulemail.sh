efa9121a2d7f24e81845404272733684f0ed52a3
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-27 12:58:30 +0530

    commit all

--- 19-schedulemail.sh
+++ 19-schedulemail.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 
-DISK_USAGE="$(df -hT | grep -v FileSystem)"
+DISK_USAGE=$(df -hT | grep -v FileSystem)
 MAX_USAGE=1
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read -r line
 do
-    CONSUMED_PERCENTAGE="$(echo $line | awk '{print $6F}' | cut -d "%" -f1 )"
-    CONSUMED_DIR="$(echo $line | awk '{print $7F}')"
+    CONSUMED_PERCENTAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1 )
+    CONSUMED_DIR=$(echo $line | awk '{print $7F}')
 
     if [ $CONSUMED_PERCENTAGE -gt $MAX_USAGE ]
     then
-        MSG+= "$CONSUMED_DIR is consumed more than 80%"
+        MSG+= $CONSUMED_DIR is consumed more than 80%
     fi
 done <<< $DISK_USAGE
 
