6b321df40d31cd36a49e80acbe872f4b37cb2f00
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-27 13:03:23 +0530

    commit all

--- 19-schedulemail.sh
+++ 19-schedulemail.sh
@@ -7,8 +7,8 @@ IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do
-    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk '{print $7F}')
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
