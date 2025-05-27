57fde9502ba06db8462f33404dcd043d34196826
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-27 12:01:50 +0530

    commit all

--- 19-schedulemail.sh
+++ 19-schedulemail.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-
-DISK_USAGE=$(df -hT | grep -v FileSystem)
-MAX_USAGE=1
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=1 # in project it will be 75
 MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do
-    CONSUMED_PERCENTAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1 )
-    CONSUMED_DIR=$(echo $line | awk '{print $7F}')
-
-    if [ $CONSUMED_PERCENTAGE -gt $MAX_USAGE ]
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+= $CONSUMED_DIR is consumed more than 80%
+        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
     fi
 done <<< $DISK_USAGE
-echo -e $MSG
 
