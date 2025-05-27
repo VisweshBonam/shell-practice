a4755a9d63e45c9dbc20ea1b85a8b9ff545ca352
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-27 11:43:58 +0530

    commit all

--- 19-schedulemail.sh
+++ 19-schedulemail.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-DISK_USAGE="$(df -hT | grep -v FileSystem)"
+DISK_USAGE=$(df -hT | grep -v FileSystem)
 MAX_USAGE=1
 MSG=""
 
 while IFS= read  line
 do
-    CONSUMED_PERCENTAGE="$(echo $line | awk '{print $6F}' | cut -d "%" -f1 )"
-    CONSUMED_DIR="$(echo $line | awk '{print $7F}')"
+    CONSUMED_PERCENTAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1 )
+    CONSUMED_DIR=$(echo $line | awk '{print $7F}')
 
     if [ $CONSUMED_PERCENTAGE -gt $MAX_USAGE ]
     then
